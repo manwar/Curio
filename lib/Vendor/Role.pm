97a425590a098557c69c6ca9110dc75eefe75f0d
Aran Clary Deltac <bluefeet@gmail.com>
2019-04-09 12:37:12 -0700

    Document Vendor::Role.

--- lib/Vendor/Role.pm
+++ lib/Vendor/Role.pm
@@ -1,22 +1,6 @@
 package Vendor::Role;
 our $VERSION = '0.01';
 
-=encoding utf8
-
-=head1 NAME
-
-Vendor::Role - Role for Vendor classes.
-
-=head1 SYNOPSIS
-
-    ...
-
-=head1 DESCRIPTION
-
-...
-
-=cut
-
 use Vendor::Meta;
 
 use Moo::Role;
@@ -31,6 +15,46 @@ sub vendor {
 
 1;
 __END__
+
+=encoding utf8
+
+=head1 NAME
+
+Vendor::Role - Role for Vendor classes.
+
+=head1 DESCRIPTION
+
+This L<Moo::Role>:
+
+=over
+
+=item *
+
+Sets up L<Exporter> so that L<Vendor/export_name> and
+L<Vendor/always_export> can work.
+
+=item *
+
+Provides the L</vendor> method, a convenient way to access
+the underlying L<Vendor::Meta> object of a vendor class.
+
+=item *
+
+Enables C<$class-E<gt>does('Vendor::Role')> checks.
+
+=back
+
+=head1 CLASS METHODS
+
+=head2 vendor
+
+    my $vendor_meta = MyApp::Cache->vendor();
+
+Returns the class's L<Vendor::Meta> object.
+
+This method may also be called on instances of the class.
+
+Calling this is equivalent to calling L<Vendor::Meta/find_meta>.
 
 =head1 SUPPORT
 
